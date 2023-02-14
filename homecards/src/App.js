7977e1f81931acb90dab5a210ac615a910d141ed
katiiisha <mendeleevakate2002@gmail.com>
2023-02-14 17:01:05 +0300

    не сдела втрое дз

--- homecards/src/App.js
+++ homecards/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Badge from './components/Badge';
 import Index from './components/Index';
 
 function App() {
@@ -8,9 +9,11 @@ function App() {
     { id: 2, type: 'thousers', price: 3000, content: 'Состоит из 100% хлопкай. Заканчилиьсь ', text: 'нет в налачии' },
     { id: 3, type: 'Jacket', price: 3500, content: 'Состоит из 100% хлопкай.', text: 'скоро поступит'   },
   ]
+  
   return (
     <div className='App'>
       <Index products={products} />
+      {/* <Badge/> */}
   </div>
   );
 }
